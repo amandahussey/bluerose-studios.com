dd6ed153748176192ad50ec0e936fd55a02a557a
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-12-08 14:58:18 -0700

    add buttons to landing

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,21 +1,21 @@
-import { Box, Stack, Typography } from "@mui/material";
-import bluerose from "/blue-rose.png";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import lolaCover from "/lola-cover.png";
+import { ArrowRightAltRounded } from "@mui/icons-material";
 
 const Landing = () => {
   return (
     <Stack alignSelf="center" mt={8} spacing={6}>
-      <Stack spacing={1}>
-        <img
+      <Stack>
+        {/* <img
           src={bluerose}
           alt="blue rose"
           style={{ height: 30, objectFit: "contain" }}
-        />
-        <Typography variant="subtitle1" textAlign="center" letterSpacing={4}>
+        /> */}
+        <Typography variant="h5" textAlign="center" letterSpacing={4}>
           blue rose studios
         </Typography>
 
-        <Typography variant="subtitle2" textAlign="center" letterSpacing={9}>
+        <Typography variant="h6" textAlign="center" letterSpacing={9}>
           presents
         </Typography>
       </Stack>
@@ -24,9 +24,34 @@ const Landing = () => {
         <img
           src={lolaCover}
           alt="Lola and the Lost Ring"
-          style={{ height: "50vh", objectFit: "contain" }}
+          style={{ height: "50vh", objectFit: "contain", borderRadius: 6 }}
         />
       </Box>
+
+      <Stack direction="row" justifyContent="center" spacing={4}>
+        <Button
+          variant="contained"
+          // href="/books/lola"
+          size="large"
+          style={{
+            background: "linear-gradient(to right bottom, #430089, #82ffa1)",
+          }}
+        >
+          Read About the Book
+        </Button>
+
+        <Button
+          variant="contained"
+          // href="/books/lola/games"
+          size="large"
+          endIcon={<ArrowRightAltRounded />}
+          style={{
+            background: "linear-gradient(to right bottom, #430089, #82ffa1)",
+          }}
+        >
+          Play Games
+        </Button>
+      </Stack>
     </Stack>
   );
 };
