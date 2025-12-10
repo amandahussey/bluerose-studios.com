ada011b108143c2ea600657dd98d9891a7636a7c
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-12-09 23:12:01 -0700

    begin mobile spot the difference

--- src/SpotTheDifference.tsx
+++ src/SpotTheDifference.tsx
@@ -15,7 +15,7 @@ const spotTheDifference2 = "/spot-the-difference-2.png";
 const SpotTheDifference = () => {
   const theme = useTheme();
 
-  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.down("md"));
 
   const [numDiffsFound, setNumDiffsFound] = useState(0);
 
@@ -33,87 +33,111 @@ const SpotTheDifference = () => {
   }, [numDiffsFound]);
 
   return (
-    <Container maxWidth="xl">
-      <Stack spacing={1} textAlign="center" pt={3}>
-        <Typography variant="h4">Spot the Difference</Typography>
-        <Typography variant="h6">Lola's Messy Room</Typography>
-        <Typography variant="h6">Tap all 7 differences to win!</Typography>
-      </Stack>
-      <Stack
-        onClick={() => {
-          setNumDiffsFound((prev) => (prev < 7 ? prev + 1 : prev));
-        }}
-      >
+    <Stack
+      flexGrow={1}
+      sx={{
+        background: `linear-gradient(to bottom, ${theme.palette.primary.dark} 70%, ${theme.palette.secondary.dark})`,
+      }}
+    >
+      <Container maxWidth="xl" sx={{ flexGrow: 1 }}>
+        <Stack spacing={1} textAlign="center" pt={3}>
+          <Typography variant="h4">Spot the Difference</Typography>
+          <Typography variant="h6">Lola's Messy Room</Typography>
+          <Typography variant="h6">Tap all 7 differences to win!</Typography>
+        </Stack>
+
         <Stack
-          mt={4}
-          width="fit-content"
-          alignSelf="center"
-          p={3}
-          style={{
-            background: `linear-gradient(to bottom, ${theme.palette.primary.dark}, ${theme.palette.secondary.main})`,
-            border: `2px solid ${theme.palette.secondary.light}`,
-            borderRadius: 8,
+          onClick={() => {
+            setNumDiffsFound((prev) => (prev < 7 ? prev + 1 : prev));
           }}
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
         >
           <Stack
-            direction={isSm ? "column" : "row"}
-            justifyContent="center"
-            spacing={3}
+            mt={4}
+            mb={8}
+            width="fit-content"
+            alignSelf="center"
+            p={isMd ? 1 : 3}
+            style={{
+              background: `linear-gradient(to bottom, ${theme.palette.primary.main}, ${theme.palette.secondary.dark})`,
+              border: isMd
+                ? "none"
+                : `2px solid ${theme.palette.secondary.light}`,
+              borderRadius: 8,
+              boxShadow: `0 0 10px ${theme.palette.secondary.light}`,
+            }}
           >
-            <img
-              src={spotTheDifference1}
-              alt={`Spot the Difference - Lola's Messy Room - Image 1`}
-              style={{
-                width: isSm ? "80vw" : "35vw",
-                objectFit: "contain",
-                borderRadius: 4,
-              }}
-            />
-            <img
-              src={spotTheDifference2}
-              alt={`Spot the Difference - Lola's Messy Room - Image 2`}
-              style={{
-                width: isSm ? "80vw" : "35vw",
-                objectFit: "contain",
-                borderRadius: 4,
-              }}
-            />
+            {isMd && (
+              <Typography variant="h6" mb={2} mt={1} textAlign="center">
+                <b>Differences found:</b> {numDiffsFound}/7
+              </Typography>
+            )}
+            <Stack
+              direction={isMd ? "column" : "row"}
+              justifyContent="center"
+              spacing={3}
+            >
+              <img
+                src={spotTheDifference1}
+                alt={`Spot the Difference - Lola's Messy Room - Image 1`}
+                style={{
+                  width: isMd ? "80vw" : "35vw",
+                  maxWidth: 500,
+                  objectFit: "contain",
+                  borderRadius: 4,
+                }}
+              />
+              <img
+                src={spotTheDifference2}
+                alt={`Spot the Difference - Lola's Messy Room - Image 2`}
+                style={{
+                  width: isMd ? "80vw" : "35vw",
+                  maxWidth: 500,
+                  objectFit: "contain",
+                  borderRadius: 4,
+                }}
+              />
+            </Stack>
+            {!isMd && (
+              <Box>
+                <Typography variant="h6" textAlign="right" mt={3}>
+                  <b>Differences found:</b> {numDiffsFound}/7
+                </Typography>
+              </Box>
+            )}
           </Stack>
-          <Box>
-            <Typography variant="h6" textAlign="right" mt={3}>
-              <b>Differences found:</b> {numDiffsFound}/7
+        </Stack>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 400,
+              boxShadow: 24,
+              p: 4,
+              background: `linear-gradient(to bottom, ${theme.palette.primary.dark}, ${theme.palette.secondary.dark})`,
+              color: theme.palette.text.primary,
+            }}
+          >
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Congratulations!
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              You win!
             </Typography>
           </Box>
-        </Stack>
-      </Stack>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            boxShadow: 24,
-            p: 4,
-            background: `linear-gradient(to bottom, ${theme.palette.primary.dark}, ${theme.palette.secondary.dark})`,
-            color: theme.palette.text.primary,
-          }}
-        >
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Congratulations!
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            You win!
-          </Typography>
-        </Box>
-      </Modal>
-    </Container>
+        </Modal>
+      </Container>
+    </Stack>
   );
 };
 
